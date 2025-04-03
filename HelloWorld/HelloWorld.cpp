a6594aeafae5a94cfb42d1c3e2d9e685e765d108
Charles Shenkir <cshenkir@student.neumont.edu>
2025-04-03 14:35:42 -0600

    lession adds

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 using namespace std;
 
+//Struct: usually holds only data, public by default
+//Class: usually holds methods, private by default
 int main() {
+    //std::cout if you dont use std namespace
     cout << "Hello, World!" << endl;
-    cout << "Completed Git Setup!" << endl;
+
+    //datatypes
+    int i = 87;
+    printf("%d\n", i);
+    cout << i << std::endl;
+    cout << sizeof(int) << std::endl;
+    cout << (char)i << endl;
+    cout << &i << endl;
+    cout << ++i << endl;
+    cout << i++ << endl;
+    cout << i << endl;
+
+    char c = 'A';
+    cout << c << endl;
+
+
     return 0;
 }
